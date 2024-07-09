9a6e316951eab52af385b8c5c10ae7fe761df69c
TheBearWPL <maksymilian_jarosz@op.pl>
2024-07-09 14:28:51 +0200

    update

--- task-1.js
+++ task-1.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalPrice = quantity * pricePerDroid; //def
+
+  if (totalPrice > customerCredits) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`; //info
+  }
+}
--- task-2.js
+++ task-2.js
@@ -1,1 +1,9 @@
 "use strict";
+
+function formatMessage(message, maxLength) {
+  if (message.length <= maxLength) {
+    return message;
+  } else {
+    return message.slice(maxLength) + "...";
+  }
+}
