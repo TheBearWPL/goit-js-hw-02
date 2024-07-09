f6b32068df7bb3dc7e6c51b75a60bab128318df0
TheBearWPL <maksymilian_jarosz@op.pl>
2024-07-09 15:19:44 +0200

    Update task-4.js

--- task-4.js
+++ task-4.js
@@ -1,1 +1,16 @@
 "use strict";
+
+function getShippingCost(country) {
+  switch (country) {
+    case "china":
+      return "Shipping to China will cost 100 credits";
+    case "chile":
+      return "Shipping to Chile will cost 250 credits";
+    case "australia":
+      return "Shipping to Australia will cost 170 credits";
+    case "jamaica":
+      return "Shipping to Jamaica will cost 120 credits";
+    else
+      return "Sorry, there is no delivery to your country";
+  }
+}
